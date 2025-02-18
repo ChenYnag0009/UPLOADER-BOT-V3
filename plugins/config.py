04f8dbcac4b261eca61f6165256bbed69426bd67
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-02-18 23:21:03 +0700

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -33,7 +33,7 @@ class Config(object):
 
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
 
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
 
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
 
